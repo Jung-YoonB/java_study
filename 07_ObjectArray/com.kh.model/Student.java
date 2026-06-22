cf7dcdfbaba5451d272d5195648636163839c60c
Jung-YoonB <amazetong@naver.com>
2026-06-22 12:47:50 +0900

    #260622 [6월 2주차 실습] Student.java
    Student 클래스 생성 코드

---
+++ 07_ObjectArray/com.kh.model/Student.java
@@ -0,0 +1,60 @@
+package com.kh.model;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	private double score;
+	
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, int age, double score) {
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+	// 알트 + 쉬프트 + s => o : 생성자 생성
+	/*
+	public Student(String name, int age, double score) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+	*/
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	public void printInfo() {
+		System.out.println("이름 : " + this.name);
+		System.out.println("나이 : " + this.age);
+		System.out.println("점수 : " + this.score);
+		System.out.println();
+	}
+	
+}
