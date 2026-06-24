485a20150652bc5e74622b1193c10a1c17c1ba7b
Jung-YoonB <amazetong@naver.com>
2026-06-24 14:50:57 +0900

    리팩토링

--- Practice9/com.kh.ex1.run/Run.java
+++ Practice9/com.kh.ex1.run/Run.java
@@ -9,20 +9,20 @@ public class Run {
 	public static void main(String[] args) {
 		RemoteControl[] rc = new RemoteControl[2];
 		rc[0] = new Television();
-		rc[1] = new Audio();
+		rc[1] = new Audio();			
 		
 		for (RemoteControl r : rc) {
+			r.turnON();
 			
 			if (r instanceof Television) {
-				r.turnON();
-				r.setVolume(7);
+				r.setVolume(7);	
 				r.turnOff();
-			System.out.println("---");
+				System.out.println("---");
 			} else if (r instanceof Audio) {
-				r.turnON();
 				r.setVolume(5);
 				r.turnOff();
 			}
+			
 		}
 		
 	}
