39deb96199284c9a50be6b3928e6c38ab0266828
Jung-YoonB <amazetong@naver.com>
2026-06-24 16:26:22 +0900

    [종합 연습 문제 9] 인터페이스를 활용하여 프로그램 만들기 - 2_Phone.java_인터페이스(추상 메소드) 생성

---
+++ Practice9/com.kh.ex2.model/Phone.java
@@ -0,0 +1,13 @@
+package com.kh.ex2.model;
+
+public interface Phone {
+	char[] NUMBERPAD = { '1', '2', '3'
+						, '4', '5', '6'
+						, '7', '8', '9'
+						, '*', '0', '#'};
+	
+	
+	String makeCall();
+	
+	String takeCall();
+}
