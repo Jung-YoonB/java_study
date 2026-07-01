7247e0f21b67f377df9a4350e0204e6ff5fba8dc
Jung-YoonB <amazetong@naver.com>
2026-07-01 17:23:34 +0900

    [종합 연습 문제 12] 기본 API를 활용하여 프로그램 만들기 - 2_Run.java

---
+++ Practice12/com.kh.ex2.run/Run.java
@@ -0,0 +1,14 @@
+package com.kh.ex2.run;
+
+import com.kh.ex2.view.StudyCafeView;
+
+public class Run {
+
+	public static void main(String[] args) {
+		StudyCafeView sv = new StudyCafeView();
+		
+		sv.runApp();
+		
+	}
+
+}
