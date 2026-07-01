9078e555f7306850d8c7140b1a5a7e39a36d8960
Jung-YoonB <amazetong@naver.com>
2026-07-01 17:18:31 +0900

    #260630 [6월 4주차 실습] Student.java_기본 테스트용

---
+++ 14_Collection/com.kh.model/Student.java
@@ -0,0 +1,58 @@
+package com.kh.model;
+
+public class Student {
+	
+	private String name;
+	private int age;
+	private double score;
+	
+	
+	public Student() {
+		
+	}
+	
+	public Student(String name, int age, double score) {
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+	// 알트 + 쉬프트 + s => o : 생성자 생성
+	/*
+	public Student(String name, int age, double score) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+	*/
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", score=" + score + "]";
+	}	
+	
+}
